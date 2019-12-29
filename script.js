2cdbb0861e87198db3b00bbc467c0707bdd677cf
sage4outlook <sage4outlook@yahoo.com>
2019-12-29 17:40:08 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,62 @@
+/*  mini facebook project
+
+var database = [
+    {
+        username: 'andrei' , 
+        password: 'supersecret'
+    }
+]
+var newsFeed = [
+    {
+        username : 'bobby',
+        timeline : 'so tired from all that learning'
+    },
+    {
+        username: 'sally',
+        timeline : 'javascript is so cool'
+    }
+]
+
+
+var userNamePrompt = prompt('what is your username?')
+var passwordPrompt = prompt('what is ur password?')
+
+var user = userNamePrompt
+var pass = passwordPrompt
+
+function signIn(user, pass) {
+    if (user === database[0].username && pass === database[0].password) {
+        console.log(newsFeed)
+    } else {
+        alert('sorry , wrong username and password')
+    }
+}
+
+signIn(user , pass) */
+
+
+/* var todos = [
+    'claen room!',
+    'brush teeth!',
+    'excercise!',
+    'study javascript!',
+    'eat healthy!'
+]
+for (var i = 0 ; i < todos.length; i+=) {
+
+} */
+
+/* var css = document.querySelector('h3')
+var color1 = document.querySelector('.color1')
+var color2 = document.querySelector('.color2')
+var body = document.getElementById('gradient') */
+
+/* body.style.background = 'red' */
+/* color1.addEventListener('input' , function(){
+    body.style.background = "linear-gradient(to right ," +color1.value+", "+color2.value+")"
+    css.textContent = body.style.background
+})
+color1.addEventListener('input' , function(){
+    body.style.background = "linear-gradient(to right ," +color1.value+", "+color2.value+")"
+    css.textContent = body.style.background
+}) */
